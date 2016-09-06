f87fdfd75816055bf4232e9bceba83b7e92a9fb7
ishansaksena <ishansaksena@gmail.com>
2016-09-06 12:54:51 +0530

    Initial commit, can pull in data, initial form UI in place

---
+++ Yelperizer/LimitViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LimitViewController.swift
+//  Yelperizer
+//
+//  Created by ishansaksena on 9/6/16.
+//  Copyright © 2016 ishansaksena. All rights reserved.
+//
+
+import UIKit
+
+class LimitViewController: UIViewController {
+
+    @IBOutlet weak var limitTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
